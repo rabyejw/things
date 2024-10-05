1a610b96591e8f7695bee3e3b34801c5d1bb116b
rabyejw <138731431+rabyejw@users.noreply.github.com>
2024-10-05 21:08:19 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,25 @@
-vim.opt.number = true;
+vim.opt.number = true
+
+vim.o.background = "dark"
+
+local highlight_groups = {
+    "Normal",
+    "Comment",
+    "Identifier",
+    "Statement",
+    "PreProc",
+    "Type",
+    "Constant",
+    "String",
+    "Special",
+    "Number",
+    "Function",
+    "Operator",
+    "Keyword",
+    "Variable",
+    "Todo",
+}
+
+for _, group in ipairs(highlight_groups) do
+    vim.cmd(string.format("highlight %s guifg=#FFFFFF ctermfg=white", group))
+end
