007bed582c4bb452058199f44dccdf3c24a24b87
rabyejw <138731431+rabyejw@users.noreply.github.com>
2024-10-06 16:34:35 +0000

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,6 +1,26 @@
 vim.opt.number = true
+-- vim.opt.relativenumber = true
 
-vim.o.background = "dark"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.colorcolumn = "80"
+
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+
+vim.opt.list = true
+										
+
+vim.opt.mouse = "a"
+
+
+
+
+
+
+
+
+-- vim.cmd("colorscheme koehler")
 
 local highlight_groups = {
     "Normal",
@@ -18,8 +38,36 @@ local highlight_groups = {
     "Keyword",
     "Variable",
     "Todo",
+    "SpecialKey",
+    "Visual",
+    "CursorLine",
+    "CursorColumn",
+    "StatusLine",
+    "LineNr",
+    "NonText",
+    "Whitespace",
+    "Error",
+    "Warning",
+    "Search",
+    "IncSearch",
+    "DiffAdd",
+    "DiffChange",
+    "DiffDelete"
 }
-
 for _, group in ipairs(highlight_groups) do
-    vim.cmd(string.format("highlight %s guifg=#FFFFFF ctermfg=white", group))
+    vim.cmd(string.format("highlight %s ctermfg=white", group))
 end
+
+
+
+
+-- What do highlight group do
+-- LineNr - changes the line numbers :set nu
+-- NonText - changes the 
+
+-- cterm options bold,underline,italic,reverse,strikethrough,standout,conceal
+
+vim.opt.background = dark
+
+vim.cmd("highlight LineNr ctermfg=grey cterm=bold")
+
